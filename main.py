79ab1123f01b1899214b0b6f0a70b83a47662578
Joschka Birk <joschka.birk@gmail.com>
2023-12-09 19:12:27 +0100

    Adding the image generation step to main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
 
 from src.alsterfood_scraping import fetch_lunch_menu
+from src.image_generation import generate_image
 from src.mattermost_posting import send_message
 
-DEBUG = False
+DEBUG = True
 ALSTERFOOD_WEBSITE_URL = os.getenv("ALSTERFOOD_WEBSITE_URL")
 MATTERMOST_WEBHOOK_URL = os.getenv("MATTERMOST_WEBHOOK_URL")
-IMAGE_URL = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-Q9Rj5vhxAlToLRKA1ArxsFPb/user-3wIkLqTRO6pY44iwB89vGS34/img-5Z3RFrZgJwdENFP2Za33DoAl.png?st=2023-12-09T15%3A41%3A53Z&se=2023-12-09T17%3A41%3A53Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-12-08T23%3A13%3A02Z&ske=2023-12-09T23%3A13%3A02Z&sks=b&skv=2021-08-06&sig=bqSjVm52XHAoMzcDEbiqvjd1enrdVSzJewY/Iz6vSE4%3D"
+
 debug_list_of_meals = [
     "Spaghetti Bolognese (Pork/Beef) with Gouda shavings",
     "Vegan Peanut Vegetable Curry with Coconut Milk and Basmati Rice",
@@ -20,8 +21,8 @@ debug_list_of_descriptions = [
     "This vegan peanut vegetable curry is made with coconut milk and basmati rice. The wide range of vegetables will make you feel like you're in a tropical paradise!",
 ]
 debug_list_of_images = [
-    IMAGE_URL,
-    IMAGE_URL,
+    "https://oaidalleapiprodscus.blob.core.windows.net/private/org-Q9Rj5vhxAlToLRKA1ArxsFPb/user-3wIkLqTRO6pY44iwB89vGS34/img-wEyVXCz1cdPfUjWI3T3he2Ek.png?st=2023-12-09T17%3A10%3A35Z&se=2023-12-09T19%3A10%3A35Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-12-08T23%3A13%3A43Z&ske=2023-12-09T23%3A13%3A43Z&sks=b&skv=2021-08-06&sig=7md/joH9I1tPhWzZ6f4YqmzXoY1FJHexuCIDUf0Z5d4%3D",
+    "https://oaidalleapiprodscus.blob.core.windows.net/private/org-Q9Rj5vhxAlToLRKA1ArxsFPb/user-3wIkLqTRO6pY44iwB89vGS34/img-5Iz1SIavniWBEeGIDXGf9Wxu.png?st=2023-12-09T17%3A10%3A42Z&se=2023-12-09T19%3A10%3A42Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-12-08T23%3A12%3A49Z&ske=2023-12-09T23%3A12%3A49Z&sks=b&skv=2021-08-06&sig=BRIxAYciSC3TlzmgXfhMJ32XWV9Y1KjqoX2swR7gfwM%3D",
 ]
 
 
@@ -37,10 +38,20 @@ if __name__ == "__main__":
 
     print(list_of_meals)
 
+    if DEBUG:
+        images = debug_list_of_images
+    else:
+        images = []
+        # Generate an image with DALL-E based on the menu entries
+        for menu_entry in list_of_meals:
+            generated_image_url = generate_image(prompt=menu_entry)
+            images.append(generated_image_url)
+            # Print the URL of the generated image
+            print(f"Generated Image URL: {generated_image_url}")
+
     # TODO: Replace this with the actual meals of the day and their descriptions
     # and images
     descriptions = debug_list_of_descriptions
-    images = debug_list_of_images
 
     prefix = f"For today, we have the following meals ({ALSTERFOOD_WEBSITE_URL}):"
 
--- src/image_generation.py
+++ src/image_generation.py
@@ -1,34 +1,34 @@
 from openai import OpenAI
 
 
-def generate_images_with_dalle(prompt):
+def generate_image(
+    prompt,
+    model="dall-e-2",
+    size="256x256",
+    quality="standard",
+):
+    """Generate images with DALL-E based on a prompt.
+
+    Parameters
+    ----------
+    prompt : str
+        The prompt to generate an image for.
+    model : str
+        The model to use for generating the image. Defaults to "dall-e-2".
+    size : str
+        The size of the generated image. Defaults to "256x256".
+    quality : str
+        The quality of the generated image. Defaults to "standard".
+    """
     client = OpenAI()
 
     response = client.images.generate(
-        model="dall-e-2",
+        model=model,
         prompt=prompt,
-        size="256x256",
-        quality="standard",
+        size=size,
+        quality=quality,
         n=1,
     )
-
     image_url = response.data[0].url
 
     return image_url
-
-
-if __name__ == "__main__":
-    # Replace the prompts with your actual list of menu entries
-    menu_entries = [
-        "Spaghetti Bolognese",
-        # "Pizza Salami",
-    ]
-
-    # Convert the list of menu entries to a string
-    prompt = "\n".join(menu_entries)
-
-    # Generate an image with DALL-E based on the menu entries
-    for menu_entry in menu_entries:
-        generated_image_url = generate_images_with_dalle(prompt)
-        # Print the URL of the generated image
-        print(f"Generated Image URL: {generated_image_url}")
